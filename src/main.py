bfa1e259c82a5994da5700fe465a28a37568a9eb
yalexaner <nrizxhDJpHiuTe7veF7NbmM2HBkvNXXfSGQkt13G1Z20aBnnExRsMny2RhF4REnPqmxGyQ4q>
2020-09-16 13:00:01 +0500

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 import argparse
 import shutil
 
-parser = argparse.ArgumentParser()
-parser.parse_args()
 
-# creating aliases
-shutil.copy('../res/.bash_aliases', '/home/yalexaner/')
+def setup_bash():
+    # creating aliases
+    shutil.copy('../res/.bash_aliases', '/home/yalexaner/')
 
-# changing promt
-with open('/home/yalexaner/.bashrc', 'a') as bashrc:
-    with open('../res/promt') as promt:
-        bashrc.write(promt.read())
+    # changing promt
+    with open('/home/yalexaner/.bashrc', 'a') as bashrc:
+        with open('../res/promt') as promt:
+            bashrc.write(promt.read())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+
+    setup_bash()
