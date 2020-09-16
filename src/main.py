5b5c419156b8a0b0b9657766a3e2104623e87053
yalexaner <mail.yalexaner@gmail.com>
2020-09-16 15:46:12 +0500

    adding directory configuration

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-import argparse
-import os
-import shutil
+from argparse import ArgumentParser
+from os import system as execute, listdir, rmdir, mkdir
+from shutil import copy as copy_file
 
 
 def setup_bash():
     # creating aliases
-    shutil.copy('../res/.bash_aliases', '/home/yalexaner/')
+    copy_file('../res/.bash_aliases', '/home/yalexaner/')
 
     # changing promt
     with open('/home/yalexaner/.bashrc', 'a') as bashrc:
@@ -13,54 +13,65 @@ def setup_bash():
             bashrc.write(promt.read())
 
 
+def configure_dirs():
+    dirs = listdir('/home/yalexaner')
+
+    for dir in dirs:
+        rmdir(f'/home/yalexaner/{dir}')
+
+    mkdir('/home/yalexaner/downloads')
+
+
 def install_and_configure_yandex_disk():
     # installing yandex-disk
-    os.system('echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" '
-              '| sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null '
-              '&& wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- '
-              '| sudo apt-key add - && sudo apt update '
-              '&& sudo apt install -y yandex-disk')
+    execute('echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" '
+            '| sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null '
+            '&& wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- '
+            '| sudo apt-key add - && sudo apt update '
+            '&& sudo apt install -y yandex-disk')
 
     # setting up yandex-disk
-    os.system('yandex-disk setup')
+    execute('yandex-disk setup')
 
 
 def install_and_configure_git():
-    os.system('sudo apt install git')
+    execute('sudo apt install git')
 
     username = input('username (leave empty for default value): ') or 'yalexaner'
     email = input('email (leave empty for default value): ') or 'mail.yalexaner@gmail.com'
 
-    os.system(f'git config --global user.name {username}')
-    os.system(f'git config --global user.email {email}')
-    os.system('git config --global user.editor vim')
+    execute(f'git config --global user.name {username}')
+    execute(f'git config --global user.email {email}')
+    execute('git config --global user.editor vim')
 
 
 def install_applications():
     # Vivaldi
-    os.system('wget "https://downloads.vivaldi.com/stable/vivaldi-stable_3.3.2022.45-1_amd64.deb" -O /tmp/vivaldi.deb '
-              '&& sudo dpkg -i /tmp/vivaldi.deb')
+    execute('wget "https://downloads.vivaldi.com/stable/vivaldi-stable_3.3.2022.45-1_amd64.deb" -O /tmp/vivaldi.deb '
+            '&& sudo dpkg -i /tmp/vivaldi.deb')
 
     # Stremio
-    os.system('wget "https://dl.strem.io/shell-linux/v4.4.116/stremio_4.4.116-1_amd64.deb" -O /tmp/stremio.deb '
-              '&& sudo dpkg -i /tmp/stremio.deb')
+    execute('wget "https://dl.strem.io/shell-linux/v4.4.116/stremio_4.4.116-1_amd64.deb" -O /tmp/stremio.deb '
+            '&& sudo dpkg -i /tmp/stremio.deb')
 
     # apts: Spotify, Telegram, Zeal, mpv, GParted, Vim, Vim GTK (for external clipboard)
-    os.system('sudo apt install spotify-client telegram-desktop zeal mpv gparted vim vim-gtk')
+    execute('sudo apt install spotify-client telegram-desktop zeal mpv gparted vim vim-gtk')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Setup the system.')
+    parser = ArgumentParser(description='Setup the system.')
 
     parser.add_argument('object', default='everything', nargs='?',
-                        choices=['everything', 'bash', 'yandex-disk', 'git', 'apps'],
-                        metavar='{basic, bash, yandex-disk, apps}',
+                        choices=['everything', 'bash', 'dirs', 'yandex-disk', 'git', 'apps'],
+                        metavar='{basic, bash, dirs, yandex-disk, apps}',
                         help='Object on which to perform the setup.')
 
     arg = parser.parse_args().object
 
     if arg == 'bash':
         setup_bash()
+    elif arg == 'dirs':
+        configure_dirs()
     elif arg == 'yandex-disk':
         install_and_configure_yandex_disk()
     elif arg == 'git':
@@ -69,6 +80,7 @@ if __name__ == '__main__':
         install_applications()
     else:
         setup_bash()
+        configure_dirs()
         install_and_configure_yandex_disk()
         install_and_configure_git()
         install_applications()
