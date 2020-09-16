3686c05d0d090150d9a39000dc5f7d534439bfd0
yalexaner <nrizxhDJpHiuTe7veF7NbmM2HBkvNXXfSGQkt13G1Z20aBnnExRsMny2RhF4REnPqmxGyQ4q>
2020-09-16 13:07:14 +0500

    adding yandex-disk' installation and setup

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import shutil
 
 
@@ -12,8 +13,21 @@ def setup_bash():
             bashrc.write(promt.read())
 
 
+def install_and_configure_yandex_disk():
+    # installing yandex-disk
+    os.system('echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" '
+              '| sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null '
+              '&& wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- '
+              '| sudo apt-key add - && sudo apt-get update '
+              '&& sudo apt-get install -y yandex-disk')
+
+    # setting up yandex-disk
+    os.system('yandex-disk setup')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.parse_args()
 
     setup_bash()
+    install_and_configure_yandex_disk()
